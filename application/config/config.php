fa0362c8190aaa122412830fbb3cf56c7cd6c00e
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-06-24 21:37:06 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -2,8 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'https://dochoitrecon.herokuapp.com/';
-
+$config['base_url'] = 'http://localhost/webshop/';
 
 $config['index_page'] = 'index.php';
 
@@ -59,7 +58,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = '/tmp';
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
