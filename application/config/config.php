4bbce1fb90a126e61697428afa07ec85eae9ecce
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-06-24 16:02:34 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -5,12 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'https://dochoitrecon.herokuapp.com/';
 
 
-$ config ['sess_do'] = 'files';
-$ config ['sess_cookie_name'] = 'ci_session';
-$ config ['sess_Exption'] = 7200;
 $ config ['sess_save_path'] = sys_get_temp_dir ();
-$ config ['sess_match_ip'] = FALSE;
-$ config ['sess_time_to_update'] = 300;
 
 
 $config['index_page'] = 'index.php';
