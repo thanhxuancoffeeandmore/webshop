91e59a084023b52b8145303055e4716df6213720
thanhxuancoffeeandmore <thanhxuancoffeeandmore>
2020-06-24 17:18:48 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -4,13 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['base_url'] = 'https://dochoitrecon.herokuapp.com/';
 
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH . 'ci_sessions/';
-$config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+ $config['sess_save_path'] = APPPATH . 'sessions/'; 
 
 
 $config['index_page'] = 'index.php';
